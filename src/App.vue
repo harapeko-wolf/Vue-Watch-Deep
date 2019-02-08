4d94db8ac538f478be348af53745f76cf867ac3a
harapeko-wolf <32303847+harapeko-wolf@users.noreply.github.com>
2019-02-08 19:36:38 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app"><WatchDeep /></div>
+</template>
+
+<script>
+import WatchDeep from "./components/WatchDeep";
+
+export default {
+  name: "App",
+  components: {
+    WatchDeep
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/WatchDeep.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li v-for="color in colors">{{ color.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WatchDeep",
+  data() {
+    return {
+      colors: [{ name: "Red" }, { name: "White" }, { name: "Blue" }]
+    };
+  },
+  watch: {
+    colors: {
+      handler: function(newValue, oldValue) {
+        console.log("Update!");
+        console.log(
+          "new: %s, old: %s ",
+          JSON.stringfy(newValue, null, "\t"),
+          JSON.stringfy(oldValue, null, "\t")
+        );
+      },
+      deep: true
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
